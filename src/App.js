83b0babe8a2c48fa05e6d34602ec6855f788d3b1
SokolAlexander <asokol1994@gmail.com>
2021-10-28 21:58:26 +0300

    lesson2

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import { useState } from "react";
 import { Message } from "./components/Message/Message";
 import { Counter } from "./components/Counter/Counter";
 import "./App.css";
+import { Form } from "./components/Form/Form";
 
 function App() {
   const [text, setText] = useState("i am a prop");
-  const [ligougkl, jghdfjhfjk] = useState({ foo: 1 });
+  const [showCounter, setShowCounter] = useState(true);
+  const [messages, setMessages] = useState(["text1", "text2"]);
 
   const handleClick = () => {
     alert("click");
     setText("123" + Math.random());
   };
 
+  const handleToggleCounter = () => {
+    setShowCounter((prevShow) => !prevShow);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        {messages.map((mes) => (
+          <div>{mes}</div>
+        ))}
         <Message message={text} onMessageClick={handleClick} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Counter />
+        <button onClick={handleToggleCounter}>
+          {showCounter ? "hide " : "show "} counter
+        </button>
+        {/* {showCounter && <Counter text={text} />} */}
+        <Form />
       </header>
     </div>
   );
